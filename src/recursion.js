d12afaf9231098229167a0576aa5d71c00dd5f35
Andrew Myronchuk <myronchukmail@gmail.com>
2024-10-16 17:17:22 -0400

    complete compress

--- src/recursion.js
+++ src/recursion.js
@@ -459,6 +459,13 @@ let letterTally = function(str, obj) {
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
 let compress = function(list) {
+  if (list.length === 0) {
+    return [];
+  }
+  if (list[0] !== list[1]) {
+    return [list[0]].concat(compress(list.slice(1)));
+  }
+  return compress(list.slice(1));
 };
 
 // 33. Augment every element in a list with a new value where each element is an array
